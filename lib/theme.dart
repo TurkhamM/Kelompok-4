f9159f87a44e96792438b99e7c74445f6f2d8aba
syhrrmdhn31 <111825453+syhrrmdhn31@users.noreply.github.com>
2022-08-30 11:23:32 +0700

    Add files via upload

---
+++ lib/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Color blue = Color(0xff2972ff);
+Color colorLight = Color(0xfffbfbfb);
+Color textBlack = Color(0xff222222);
+Color textGrey = Color(0xff94959b);
+Color textWhiteGrey = Color(0xfff1f1f5);
+
+TextStyle judul2 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle judul5 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle judul6 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle regular16pt = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
