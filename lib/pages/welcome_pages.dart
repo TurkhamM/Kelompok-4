d4dfe754bfac2343cab413c92ff4a6b8e7c67c13
unknown <msyahruramadhan10@gmail.com>
2022-08-31 05:33:01 +0700

    menambah halaman registrasi

--- lib/pages/welcome_pages.dart
+++ lib/pages/welcome_pages.dart
@@ -1,1 +1,15 @@
 part of 'pages.dart';
+
+class wellcomePage extends StatelessWidget {
+  const wellcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: ListView(),
+      ),
+    );
+  }
+}
