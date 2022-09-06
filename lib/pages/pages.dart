0ef61cca7372a156377c49dd65e06b9b246d294d
unknown <msyahruramadhan10@gmail.com>
2022-09-06 10:57:18 +0700

    menambah fitur registrasi

--- lib/pages/pages.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-part 'welcome_pages.dart';
